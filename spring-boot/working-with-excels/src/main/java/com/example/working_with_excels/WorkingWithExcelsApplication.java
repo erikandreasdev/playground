83ff5195873875bedfa331bf774e1c1b677fff1b
Erik Andreas Barreto de Vera <erikandreasdev@proton.me>
2026-01-15 16:11:24 +0000

    feat: scaffold new Spring Boot application with Maven wrapper and initial configuration.

---
+++ spring-boot/working-with-excels/src/main/java/com/example/working_with_excels/WorkingWithExcelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.working_with_excels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithExcelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithExcelsApplication.class, args);
+	}
+
+}
---
+++ spring-boot/working-with-excels/src/test/java/com/example/working_with_excels/WorkingWithExcelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.working_with_excels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithExcelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
